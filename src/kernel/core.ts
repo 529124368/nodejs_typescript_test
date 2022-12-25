f7c9b315b1d0a76f41491e8c95f6ef4605e05846
529124368 <529124368@qq.com>
2022-12-25 23:49:51 +0900

    add socketio

--- src/kernel/core.ts
+++ src/kernel/core.ts
@@ -1,25 +1,54 @@
-import express, { Application, Request, Response } from "express"
+import express from "express"
+import http from "http"
+import socketIo from "socket.io"
 
 export class Core {
-    port: number | undefined
-    instance: Application | undefined
+    port!: number
     constructor(port: number) {
         this.port = port
     }
     run(): void {
-        this.instance = express()
-        this.instance.get("/", (req: Request, res: Response) => {
-            console.log(req.ip)
-            res.send("吨吨 你今天发骚38度是不是")
-        });
-        this.instance.get("/a", (req: Request, res: Response) => {
-            res.send("a hello world")
-        });
-        this.instance.get("/b", (req: Request, res: Response) => {
-            res.send("b hello world")
-        });
-        this.instance.listen(this.port, () => {
-            console.log("服务器启动了")
+        const ex = express()
+        const httpServer = new http.Server(ex)
+        const socketServer = new socketIo.Server(httpServer, {
+            cors: {
+                origin: "*"
+            }
+        })
+        httpServer.listen(this.port, () => {
+            console.log("服务器启动了！监听端口是:" + this.port)
+        })
+
+
+        socketServer.on("connection", (socket) => {
+            console.log(`
+有客户端链接:${socket.id}
+当前在线人数:${socketServer.engine.clientsCount}
+            `)
+            //监听
+            socket.on("message", (msg) => {
+                console.log("广播客户端=>" + socket.id + "say:" + msg)
+                socket.broadcast.emit("message", msg)
+            })
+
+            //掉线
+            socket.on("disconnect", (reason) => {
+                console.log(`
+#####START#####
+客户端:${socket.id}断开了链接
+当前在线人数:${socketServer.engine.clientsCount}
+原因是${reason}
+#####END#####
+                `)
+            })
+        })
+
+        //http
+        ex.get("/", (_, res) => {
+            res.send("hello world")
+        })
+        ex.get("/test", (_, res) => {
+            res.send("hello world test")
         })
     }
 }
